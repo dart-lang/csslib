ff875c712d6dbf94998362bd6dda6fc1e345baa0
Leon Senft <leonsenft@google.com>
2017-05-08 14:19:30 -0700

    Adds support for vendor-prefixed calc()
    Fixes #45.

--- test/declaration_test.dart
+++ test/declaration_test.dart
@@ -1260,6 +1260,20 @@ void selectorWithCalcs() {
   expect(prettyPrint(stylesheet), generated);
 }
 
+void vendorPrefixedCalc() {
+  var css = '''
+.foo {
+  width: -webkit-calc((100% - 15px*1) / 1);
+}''';
+  expectCss(css, css);
+
+  css = '''
+.foo {
+  width: -moz-calc((100% - 15px*1) / 1);
+}''';
+  expectCss(css, css);
+}
+
 main() {
   test('Simple Terms', testSimpleTerms);
   test('Declarations', testDeclarations);
@@ -1286,5 +1300,6 @@ main() {
     test('single complex', complexCalc);
     test('two calc terms for same declaration', twoCalcs);
     test('selector with many calc declarations', selectorWithCalcs);
+    test('vendor prefixed calc', vendorPrefixedCalc);
   });
 }

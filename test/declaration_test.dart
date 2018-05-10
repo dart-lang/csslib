5f0a00757039f9828e8aecc6438e1421c8decbe6
Leon Senft <leonsenft@google.com>
2018-05-10 14:09:44 -0700

    Improves compact output for @page at-rule
    Also adds test cases to verify compact output of other at-rules is optimal.

--- test/declaration_test.dart
+++ test/declaration_test.dart
@@ -857,10 +857,32 @@ div {
 .foo p[bar] {
   color: blue;
 }
+@page {
+  @top-left {
+    color: red;
+  }
+}
+@page : first{}
+@page foo : first {}
+@media screen AND (max-width: 800px) {
+  div {
+    font-size: 24px;
+  }
+}
+@keyframes foo {
+  0% {
+    transform: scaleX(0);
+  }
+}
 ''';
   final String generated =
       'div{color:green!important;background:red blue green;}'
-      '.foo p[bar]{color:blue;}';
+      '.foo p[bar]{color:blue;}'
+      '@page{@top-left{color:red;}}'
+      '@page:first{}'
+      '@page foo:first{}'
+      '@media screen AND (max-width:800px){div{font-size:24px;}}'
+      '@keyframes foo{0%{transform:scaleX(0);}}';
 
   var stylesheet = parseCss(input, errors: errors);
 
